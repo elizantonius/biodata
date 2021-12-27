94e40482c4ae2b5d329c825504e36b0ea6660928
elizantonius <lonecode1777@gmail.com>
2021-12-28 04:26:25 +0900

    first Commit

---
+++ src/com/biodata/biodata.java
@@ -0,0 +1,60 @@
+package com.biodata;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class biodata {
+
+    private JPanel panelutama;
+    private JLabel namaMhs;
+    private JLabel judul;
+    private JTextField inptnpm;
+    private JButton proses;
+    private JTextField inptnama;
+    private JLabel npm;
+    private JLabel jurusan;
+    private JLabel alamat;
+    private JTextField inptjurusan;
+    private JTextField inptalamat;
+    private JTextArea hasil;
+
+    public biodata() {
+        proses.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //Mengambil data Inputan
+                inptnpm.setText(inptnpm.getText());
+                inptnama.setText(inptnama.getText());
+                inptjurusan.setText(inptjurusan.getText());
+                inptalamat.setText(inptalamat.getText());
+
+                //Menampilkan hasil ke textarea
+                hasil.setText("NPM\t = " + inptnpm.getText()
+                             +"\n Nama Mahasiswa\t = " + inptnama.getText()
+                             +"\n Jurusan\t = " + inptjurusan.getText()
+                             +"\n Alamat\t = " + inptalamat.getText());
+
+                inptnpm.setEnabled(false);
+                inptnama.setEnabled(false);
+                inptjurusan.setEnabled(false);
+                inptalamat.setEnabled(false);
+
+
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+    public static void main(String[] args) {
+        JFrame elton = new JFrame("biodata");
+        elton.setContentPane(new biodata() .panelutama);
+        elton.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        elton.pack();
+        elton.setVisible(true);
+    }
+}
